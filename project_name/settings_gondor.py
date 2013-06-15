4afd617ae4a8a1e1ed95e0dea3104f24a20bc7a6
Brian Rosner <brosner@gmail.com>
2013-06-15 09:43:15 -0600

    updated *_URL comments in settings_gondor.py

--- project_name/settings_gondor.py
+++ project_name/settings_gondor.py
@@ -16,9 +16,11 @@ SITE_ID = 1 # set this to match your Sites setup
 MEDIA_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "media")
 STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "static")
 
-MEDIA_URL = "/site_media/media/" # make sure this maps inside of site_media_url
-STATIC_URL = "/site_media/static/" # make sure this maps inside of site_media_url
-ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
+# These two settings define what URLs your application knows about where
+# servable files are located. Make sure the URLs used here are mapped in
+# your gondor.yml under static_files.
+MEDIA_URL = "/site_media/media/"
+STATIC_URL = "/site_media/static/"
 
 FILE_UPLOAD_PERMISSIONS = 0640
 
